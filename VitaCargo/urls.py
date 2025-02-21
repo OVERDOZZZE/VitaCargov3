d26fd4c441c573731786085c5bea4ac7ee5877ad
Nurdan <saparbekov113@gmail.com>
2025-02-20 20:50:15 -0600

    commit

--- VitaCargo/urls.py
+++ VitaCargo/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import set_language
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    path('', include('profile.urls'))
+    path('', include('profile.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
